8fcbb5e72c5acffc17e1990ba4bcbe21d320aee0
Batuhan Soyarslan <batihansoyarslan50@gmail.com>
2024-07-07 15:58:47 +0300

    .

--- index.js
+++ index.js
@@ -17,12 +17,28 @@ upAnimationElements.forEach(element => {
 });
 
 
+const texts = [ "Frontend Development", "Web Development",  "Creative Coding"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
 
-// // Yeni durum objesi oluşturma
-// var stateObj = { foo: "bar" };
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
 
-// // URL'yi değiştirme
-// history.pushState(stateObj, "Yeni Başlık", "/BKDevs");
+    document.getElementById('animated-text').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 1000); 
+    } else {
+        setTimeout(type, 50); 
+    }
+})();
 
 
 
